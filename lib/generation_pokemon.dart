3fb5e26c11e4881cb7d18a42d91b602f53ec9df3
Neeraj Ghodla <neerajghodla111@gmail.com>
2019-02-28 23:11:04 +1300

    All files added.

---
+++ lib/generation_pokemon.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/pokemon_detail.dart';
+
+class ShowGenerationPokemon extends StatelessWidget {
+  final String region;
+  final List<String> imagePaths;
+
+  ShowGenerationPokemon(this.region, this.imagePaths);
+
+  /*
+  generation 1 : 1 - 151
+  generation 2 : 152 - 251
+  generation 3 : 252 - 386
+  generation 4 : 387 - 493
+  generation 5 : 494 - 649
+  generation 6 : 650 - 721
+  */
+
+  final List<String> regions = [
+    "All Pokemons",
+    "Kanto Region",
+    "Jhoto Region",
+    "Hoenn Region",
+    "Sinnoh Region",
+    "Unova Region",
+    "Kalos Region",
+  ];
+
+
+
+  List<String> getGeneration(region) {
+    List<String> names;
+    if (region == "All Pokemons") {
+      names = imagePaths;
+    } else if (region == "Kanto Region") {
+      names = imagePaths.sublist(0, 151);
+    } else if (region == "Jhoto Region") {
+      names = imagePaths.sublist(151, 251);
+    } else if (region == "Hoenn Region") {
+      names = imagePaths.sublist(251, 386);
+    } else if (region == "Sinnoh Region") {
+      names = imagePaths.sublist(386, 493);
+    } else if (region == "Unova Region") {
+      names = imagePaths.sublist(493, 649);
+    } else if (region == "Kalos Region") {
+      names = imagePaths.sublist(649, 721);
+    }
+
+    int index = regions.indexOf(region);
+    regions.removeAt(index);
+
+    return names;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> names = getGeneration(region);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          region,
+        ),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text("Categories"),
+            ),
+            Column(
+              children: regions
+                  .map((g) => ListTile(
+                        title: Text(
+                          g,
+                        ),
+                        onTap: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  ShowGenerationPokemon(g, imagePaths),
+                            ),
+                          );
+                        },
+                      ))
+                  .toList(),
+            )
+          ],
+        ),
+      ),
+      body: GridView.count(
+        primary: true,
+        padding: EdgeInsets.all(1.0),
+        crossAxisCount: 2,
+        childAspectRatio: 1.0,
+        mainAxisSpacing: 1.0,
+        crossAxisSpacing: 1.0,
+        children: pokemonCard(context, names),
+      ),
+    );
+  }
+
+  List<Widget> pokemonCard(context, List<String> names) {
+    return names
+        .map(
+          (name) => GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PokemonPage(
+                            "images/400x400/$name",
+                            name,
+                          ),
+                    ),
+                  );
+                },
+                child: Card(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                      ),
+                      Hero(
+                          tag: name,
+                          child: Image.asset("images/100x100/$name")),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                      ),
+                      Text(
+                        "${name.substring(3, name.length - 4)}",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+        )
+        .toList();
+  }
+}
---
+++ lib/pokemon_detail.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+class PokemonPage extends StatelessWidget {
+  final String imagePath;
+  final String pokemonName;
+
+  PokemonPage(this.imagePath, this.pokemonName);
+
+  final List data = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(pokemonName.substring(3, pokemonName.length - 4)),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Center(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                ),
+                Container(
+                  width: 250,
+                  height: 250,
+                  child: Hero(
+                    tag: pokemonName,
+                    child: Image.asset(
+                      imagePath,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                ),
+                Text(
+                  pokemonName.substring(3, pokemonName.length - 4),
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                ),
+                Container(
+                  child: FutureBuilder(
+                    future: DefaultAssetBundle.of(context)
+                        .loadString("assets/pokedex.json"),
+                    builder: (context, snapshot) {
+                      var pokedex = json.decode(snapshot.data.toString());
+
+                      return pokedex != null
+                          ? Container(
+                              child: pokemonStats(pokedex),
+                            )
+                          : Container();
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget pokemonStats(pokedex) {
+    String name = pokemonName.substring(0, 3);
+    int pokemonID;
+
+    if (name.substring(0, 2) == "00") {
+      pokemonID = int.parse(name.substring(2, 3)) - 1;
+    } else if (name.substring(0, 1) == "0") {
+      pokemonID = int.parse(name.substring(1, 3)) - 1;
+    } else {
+      pokemonID = int.parse(name) - 1;
+    }
+
+    return Column(
+      children: <Widget>[
+        Text(
+          "Type: ${pokedex[pokemonID]['type']}",
+          style: TextStyle(fontSize: 18),
+        ),
+        Padding(
+          padding: EdgeInsets.all(5.0),
+        ),
+        Text(
+          "Stats",
+          style: TextStyle(
+            fontSize: 19,
+            fontWeight: FontWeight.bold
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Padding(
+          padding: EdgeInsets.all(5.0),
+        ),
+        Text(
+          "HP: ${pokedex[pokemonID]["base"]["HP"]}",
+          style: TextStyle(fontSize: 18),
+        ),
+        Text(
+          "Attack: ${pokedex[pokemonID]["base"]["Attack"]}",
+          style: TextStyle(fontSize: 18),
+        ),
+        Text(
+          "Defense: ${pokedex[pokemonID]["base"]["Defense"]}",
+          style: TextStyle(fontSize: 18),
+        ),
+        Text(
+          "Sp. Attack: ${pokedex[pokemonID]["base"]["Sp. Attack"]}",
+          style: TextStyle(fontSize: 18),
+        ),
+        Text(
+          "Sp. Defense: ${pokedex[pokemonID]["base"]["Sp. Defense"]}",
+          style: TextStyle(fontSize: 18),
+        ),
+        Text(
+          "Speed: ${pokedex[pokemonID]["base"]["Speed"]}",
+          style: TextStyle(fontSize: 18),
+        ),
+      ],
+    );
+  }
+}
